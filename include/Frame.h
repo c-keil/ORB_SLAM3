c-keil/ORB_SLAM3

commit 16c779d97fd65a400d7fb24b5f2dcc625fab0cd8

updated frame to pass filenames

include/Frame.h
@@ -71,7 +71,7 @@ public:
     // ~Frame();
 
     // Extract ORB on the image. 0 for left image and 1 for right image.
-    void ExtractORB(int flag, const cv::Mat &im, const int x0, const int x1);
+    void ExtractORB(int flag, const cv::Mat &im, const int x0, const int x1, string kpFilename = "", string descFilename = "");
 
     // Compute Bag of Words representation.
     void ComputeBoW();

src/Frame.cc
@@ -416,11 +416,11 @@ void Frame::AssignFeaturesToGrid()
 }
 
 //TODO modify for IRPoint
-void Frame::ExtractORB(int flag, const cv::Mat &im, const int x0, const int x1)
+void Frame::ExtractORB(int flag, const cv::Mat &im, const int x0, const int x1, string kpFilename, string descFilename)
 {
     vector<int> vLapping = {x0,x1};
     if(flag==0)
-        monoLeft = (*mpORBextractorLeft)(im,cv::Mat(),mvKeys,mDescriptors,vLapping);
+        monoLeft = (*mpORBextractorLeft)(im,cv::Mat(),mvKeys,mDescriptors,vLapping, kpFilename, descFilename);
     else
         monoRight = (*mpORBextractorRight)(im,cv::Mat(),mvKeysRight,mDescriptorsRight,vLapping);
 }

include/ORBextractor.h
@@ -19,6 +19,7 @@
 #ifndef ORBEXTRACTOR_H
 #define ORBEXTRACTOR_H
 
+#include <string>
 #include <vector>
 #include <list>
 #include <opencv2/opencv.hpp>
@@ -58,8 +59,8 @@ public:
                    std::vector<cv::KeyPoint> &_keypoints,
                    cv::OutputArray _descriptors,
                    std::vector<int> &vLappingArea, 
-                   string kpFilename = "", 
-                   string descFilename = "");
+                   std::string kpFilename = "", 
+                   std::string descFilename = "");
 
     int inline GetLevels(){
         return nlevels;}

commit ccbfffc14c42dc032f9c21ed17f647076cbe632e

Fix bug introduced by 16c779d

include/Frame.h
@@ -21,6 +21,7 @@
 #define FRAME_H
 
 #include<vector>
+#include<string>
 
 #include "Thirdparty/DBoW2/DBoW2/BowVector.h"
 #include "Thirdparty/DBoW2/DBoW2/FeatureVector.h"
@@ -71,7 +72,7 @@ public:
     // ~Frame();
 
     // Extract ORB on the image. 0 for left image and 1 for right image.
-    void ExtractORB(int flag, const cv::Mat &im, const int x0, const int x1, string kpFilename = "", string descFilename = "");
+    void ExtractORB(int flag, const cv::Mat &im, const int x0, const int x1);//, std::string kpFilename = "", std::string descFilename = "");
 
     // Compute Bag of Words representation.
     void ComputeBoW();

src/Frame.cc
@@ -416,11 +416,11 @@ void Frame::AssignFeaturesToGrid()
 }
 
 //TODO modify for IRPoint
-void Frame::ExtractORB(int flag, const cv::Mat &im, const int x0, const int x1, string kpFilename, string descFilename)
+void Frame::ExtractORB(int flag, const cv::Mat &im, const int x0, const int x1)
 {
     vector<int> vLapping = {x0,x1};
     if(flag==0)
-        monoLeft = (*mpORBextractorLeft)(im,cv::Mat(),mvKeys,mDescriptors,vLapping, kpFilename, descFilename);
+        monoLeft = (*mpORBextractorLeft)(im,cv::Mat(),mvKeys,mDescriptors,vLapping,irkpFilename,irdescFilename);
     else
         monoRight = (*mpORBextractorRight)(im,cv::Mat(),mvKeysRight,mDescriptorsRight,vLapping);
 }